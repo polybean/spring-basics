d693aca6cad8d6565c98950cafca54d9ef76c055
polybean <polybean@gmail.com>
2019-11-08 23:57:47 +0800

    Switch to XML based configuration

--- src/main/java/com/example/RunDemo.java
+++ src/main/java/com/example/RunDemo.java
@@ -2,12 +2,13 @@ package com.example;
 
 import com.example.config.AppConfig;
 import com.example.entities.Game;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class RunDemo {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         Game game = context.getBean("game", Game.class);
         String winner = game.playGame();
