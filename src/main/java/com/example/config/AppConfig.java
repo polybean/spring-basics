fc125ae6009f1a1b179a0c435320ef65764954bc
polybean <polybean@gmail.com>
2019-11-08 23:39:07 +0800

    @Import -> @ComponentScan

--- src/main/java/com/example/config/AppConfig.java
+++ src/main/java/com/example/config/AppConfig.java
@@ -3,13 +3,14 @@ package com.example.config;
 import com.example.entities.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import javax.sql.DataSource;
 
 @Configuration
-@Import(InfrastructureConfig.class)
+@ComponentScan(basePackages = "com.example")
 public class AppConfig {
     @Autowired
     private DataSource dataSource;
