b3421f25a6e0888ff0c18b7f8b0d1ca305d2d451
polybean <polybean@gmail.com>
2019-11-08 23:33:53 +0800

    Use Autowired

--- src/main/java/com/example/config/AppConfig.java
+++ src/main/java/com/example/config/AppConfig.java
@@ -1,14 +1,24 @@
 package com.example.config;
 
 import com.example.entities.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import javax.sql.DataSource;
 
 @Configuration
+@Import(InfrastructureConfig.class)
 public class AppConfig {
+    @Autowired
+    private DataSource dataSource;
+
     @Bean
     public Game game() { // The bean is named "game"
-        return new BaseballGame(redSox(), cubs());
+        BaseballGame baseballGame = new BaseballGame(redSox(), cubs());
+        baseballGame.setDataSource(dataSource);
+        return baseballGame;
     }
 
     @Bean
